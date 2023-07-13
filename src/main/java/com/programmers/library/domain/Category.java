9d66f1221cfb00ba33bd75980e0d5b515090e2ea
yumyeonghan <mgtmh991013@gmail.com>
2023-07-13 20:56:25 +0900

    feat: Category, ReserveStatus, SeatStatus 구현

--- src/main/java/com/programmers/library/domain/Category.java
+++ src/main/java/com/programmers/library/domain/Category.java
@@ -1,4 +1,7 @@
 package com.programmers.library.domain;
 
 public enum Category {
+    OPEN,
+    CLOSED,
+    ;
 }
--- src/main/java/com/programmers/library/domain/SeatStatus.java
+++ src/main/java/com/programmers/library/domain/SeatStatus.java
@@ -1,4 +1,7 @@
 package com.programmers.library.domain;
 
 public enum SeatStatus {
+    RESERVATION_POSSIBLE,
+    RESERVATION_NOT_POSSIBLE
+    ;
 }
