c6f9fb6fe8374417c00794afb0c93308276f0108
yumyeonghan <mgtmh991013@gmail.com>
2023-07-18 17:17:49 +0900

    feat: SeatController 구현

---
+++ src/main/java/com/programmers/library/controller/seat/SeatController.java
@@ -0,0 +1,37 @@
+package com.programmers.library.controller.seat;
+
+import com.programmers.library.dto.seat.request.SeatCreateRequestDto;
+import com.programmers.library.dto.seat.response.SeatResponseDto;
+import com.programmers.library.service.seat.SeatService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class SeatController {
+
+    private final SeatService seatService;
+
+    @GetMapping("/seats")
+    public String seatsPage(Model model) {
+        List<SeatResponseDto> seatList = seatService.findSeatList();
+        model.addAttribute("seatList", seatList);
+        return "seat_list";
+    }
+
+    @GetMapping("/new-seat")
+    public String newSeatPage() {
+        return "new_seat";
+    }
+
+    @PostMapping("/seats")
+    public String newSeat(SeatCreateRequestDto seatCreateRequestDto) {
+        seatService.createSeat(seatCreateRequestDto);
+        return "redirect:/seats";
+    }
+}
