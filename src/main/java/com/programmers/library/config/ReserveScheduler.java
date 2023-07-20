8a0a96951205b0bc67b85d655c18d8107646d3c9
yumyeonghan <mgtmh991013@gmail.com>
2023-07-20 17:04:50 +0900

    feat: 스케쥴링 기능 추가

---
+++ src/main/java/com/programmers/library/config/ReserveScheduler.java
@@ -0,0 +1,20 @@
+package com.programmers.library.config;
+
+import com.programmers.library.repository.reserve.JdbcReserveRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableScheduling
+@RequiredArgsConstructor
+public class ReserveScheduler {
+
+    private final JdbcReserveRepository jdbcReserveRepository;
+
+    @Scheduled(cron = "0 0 0 * * ?") // logic to be executed 'every day at 00:00'
+    public void initReservation() {
+        jdbcReserveRepository.deleteAll();
+    }
+}
