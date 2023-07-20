56006169ce3a2d81436d37e573b2396ca2da6c32
yumyeonghan <mgtmh991013@gmail.com>
2023-07-20 16:46:41 +0900

    test: seatListByPage() 테스트 코드 작성

--- src/test/java/com/programmers/library/controller/api/seat/SeatApiControllerTest.java
+++ src/test/java/com/programmers/library/controller/api/seat/SeatApiControllerTest.java
@@ -1,5 +1,8 @@
 package com.programmers.library.controller.api.seat;
 
+import com.programmers.library.domain.seat.Category;
+import com.programmers.library.domain.seat.SeatStatus;
+import com.programmers.library.dto.seat.response.PageSeatResponseDto;
 import com.programmers.library.dto.seat.response.SeatResponseDto;
 import com.programmers.library.service.seat.SeatService;
 import org.junit.jupiter.api.DisplayName;
@@ -44,5 +47,33 @@ class SeatApiControllerTest {
 
         //then
         verify(seatService, times(1)).findSeatList();
+    }
+
+    @DisplayName("페이지별로 좌석 목록과 전체 페이지 수를 조회할 수 있다.")
+    @Test
+    void seatListByPage() throws Exception{
+        //given
+        ArrayList<SeatResponseDto> seatList = new ArrayList<>();
+        seatList.add(new SeatResponseDto(3l, Category.OPEN.getDescription(), SeatStatus.RESERVATION_POSSIBLE.getDescription()));
+        int totalPages = 3;
+        PageSeatResponseDto pageSeatResponseDto = new PageSeatResponseDto(seatList, totalPages);
+
+        int pageNumber = 2;
+        int pageSize = 2;
+        when(seatService.findSeatListByPage(pageNumber, pageSize)).thenReturn(pageSeatResponseDto);
+
+        //when
+        mockMvc.perform(get("/api/v1/page-seats?page=2&size=2"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.seats", hasSize(1)))
+                .andExpect(jsonPath("$.seats[0].seatId").value(3L))
+                .andExpect(jsonPath("$.seats[0].category").value("개방형"))
+                .andExpect(jsonPath("$.seats[0].seatStatus").value("예약가능"))
+                .andExpect(jsonPath("$.totalPages").value(totalPages))
+                .andDo(print());
+
+        //then
+        verify(seatService, times(1)).findSeatListByPage(pageNumber, pageSize);
     }
 }
